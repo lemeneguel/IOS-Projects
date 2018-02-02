e2460ff7c9f7262365be20a3d8063405a69518ab
Leandro Meneguel <lecomp@icloud.com>
2018-02-02 14:52:49 -0500

    Including a second Int variable

---
+++ Simple Button/Simple Button/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Simple Button
+//
+//  Created by Leandro Meneguel on 2/2/18.
+//  Copyright © 2018 Leandro Meneguel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
